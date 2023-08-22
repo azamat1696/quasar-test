8695f569639528e7765e18d656867d06e0f20517
AZAMAT YADYGEROV <73704375+azamat1696@users.noreply.github.com>
2023-08-22 23:43:33 +0300

    Update IndexPage.vue

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,11 @@
 <template>
   <q-page class="flex flex-center">
     <img
-      alt="Quasar logo"
+      alt="Quasar logo1212121212"
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
     >
+    12122121212
   </q-page>
 </template>
 
